1a5d4c85418d3e6f91119632ab6a418a43dd52f4
Habib-4-Tech <rudra.habib@gmail.com>
2024-01-10 14:07:58 +0600

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,35 @@
 "use strict";
+// string
 let lname;
 lname = 'John';
 let newname = lname.toUpperCase();
 console.log(newname);
+//number
+let age;
+age = 25;
+let dob = '35';
+let result = parseInt(dob); // the result variable is of number type through type inference
+//boolean
+//array
+let empList;
+empList = ['Quagmire', 'Joe', 'Peter'];
+let numlist;
+numlist = [1, 2, 3, 4, 5];
+let results = numlist.filter((num) => num > 2);
+console.log(results);
+let sum = numlist.reduce((acc, num) => acc + num);
+console.log(sum);
+//enum  --- can also be defined as constant
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Blue;
+//Tupples
+let swapNumbs;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNumbs = swapNumbers(10, 20);
--- datatypes.ts
+++ datatypes.ts
@@ -1,3 +1,4 @@
+// string
 
 let lname : string;
 
@@ -7,14 +8,67 @@ let lname : string;
 
  console.log(newname);
 
+
+ //number
+
  let age : number;
-
  age = 25;
-
-let dob = '35'
-
+ let dob = '35'
  let result = parseInt(dob);  // the result variable is of number type through type inference
 
- 
+//boolean
+
+
+let choose : boolean = false;
+
+
+ //array
+
+ let empList : string[];
+
+ empList = ['Quagmire','Joe','Peter'];
+
+ let numlist : Array<number>;
+
+ numlist = [1,2,3,4,5];
+
+ let results = numlist.filter((num)=>num>2);  
+
+ console.log(results);
+
+ let sum = numlist.reduce((acc,num) => acc + num);
+
+ console.log(sum);
+
+
+
+//enum  --- can also be defined as constant
+
+enum Color{
+
+Red,
+Green,
+Blue
+
+}
+
+let c : Color = Color.Blue;
+
+//Tupples -- can store data of various types
+
+let swapNumbs : [firstNumber:number, secondNumber:number];
+
+function swapNumbers(num1:number, num2:number) : [number,number]{
+
+    return [num2, num1];
+}
+swapNumbs = swapNumbers(10,20);
+
+// any (any types can be used/stored)
+
+
+// never
+
+//void
 
 
