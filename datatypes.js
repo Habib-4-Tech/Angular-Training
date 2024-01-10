627f69e482fb8b609abacfe96d2eb4293aa5b9a7
Habib-4-Tech <rudra.habib@gmail.com>
2024-01-10 10:04:21 +0600

    first commit

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+let lname;
+lname = 'John';
+let newname = lname.toUpperCase();
+console.log(newname);
---
+++ datatypes.ts
@@ -0,0 +1,20 @@
+
+let lname : string;
+
+ lname = 'John';
+
+ let newname = lname.toUpperCase();
+
+ console.log(newname);
+
+ let age : number;
+
+ age = 25;
+
+let dob = '35'
+
+ let result = parseInt(dob);  // the result variable is of number type through type inference
+
+ 
+
+
